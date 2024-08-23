bce9fc3ecd94900789e3c2bb7f0063ea15c687be
Diego Alejandro Montiel Florez <88790286+OugMontiel@users.noreply.github.com>
2024-08-22 20:28:55 -0500

    feat: :construction: primeras adecuaciones de la paguinacion !!
    se deja a la Ia para que la mejore

--- src/App.vue
+++ src/App.vue
@@ -15,14 +15,39 @@ import der from './components/der.vue'
 </template>
 
 <style scoped>
-main{
+* {
+  padding: 0;
+  margin: 0;
+  border: 0;
+  box-sizing: border-box;
+}
+
+main {
   display: grid;
   grid-template-columns: 2fr 7fr;
   gap: 2em
 }
-div{
-  background-color: aqua;
+
+div {
+  background-color: var(--vt-c-black-font);
+  border-radius: 1em;
+  border: .05px solid var(--vt-c-indigo);
 }
+
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+
+li {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+}
+
 /* @media (min-width: 1024px) {
 } */
 </style>
--- src/components/izq.vue
+++ src/components/izq.vue
@@ -3,9 +3,34 @@
 </script>
 
 <template>
-<h1>Izquierda</h1>
+    <main>
+        <div>
+            <img src="/public/img/Data Vizualizacion-SQL-R Studio-Power BI.jpg" alt="Data_Science_Backend Developer_Economist.png">
+            <h2>Diego Alejandro</h2>
+            <h2>Montiel Florez</h2>
+            <span>
+                <p>Data Science</p>
+                <p>Backend Developer</p>
+                <p>Economist</p>
+            </span>
+        </div> 
+    </main>
 </template>
 
 <style scoped>
-
+main{
+    padding: 1em
+}
+img {
+  width: 100%;
+  object-fit: cover;
+}
+span{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+p{
+    display: block;
+}
 </style>
