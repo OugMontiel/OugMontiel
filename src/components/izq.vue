56dd1ee36b2014cf2a7c310d071317e56ae99249
Diego Alejandro Montiel Florez <88790286+OugMontiel@users.noreply.github.com>
2024-08-23 18:22:01 -0500

    feat: :sparkles: Adecuacion del primera seccion de izq

--- src/components/izq.vue
+++ src/components/izq.vue
@@ -3,40 +3,35 @@
 </script>
 
 <template>
-    <main>
-        <section class="profile">
-            <img class="profile-image" src="/public/img/Data Vizualizacion-SQL-R Studio-Power BI.jpg" alt="Data Science, Backend Developer, Economist">
-            <h2>Diego Alejandro</h2>
-            <h2>Montiel Florez</h2>
-            <div class="professions">
-                <p>Data Science</p>
-                <p>Backend Developer</p>
-                <p>Economist</p>
-            </div>
-        </section> 
-    </main>
+
+    <section class="profile">
+        <img class="profile-image" 
+            src="/public/img/Data Vizualizacion-SQL-R Studio-Power BI.jpg"
+            alt="Data Science, Backend Developer, Economist">
+        <h2>Diego Alejandro Montiel Florez</h2>
+        <div class="professions">
+            <p>Data Science</p>
+            <p>Backend Developer</p>
+            <p>Economist</p>
+        </div>
+    </section>
+
 </template>
 
 <style scoped>
-main {
-    padding: 1em;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh; /* Asegura que el contenido ocupe toda la altura de la ventana */
-}
-
 .profile {
+    padding: 1em;
     text-align: center;
-    padding: 2em;
     border-radius: 10px;
 }
 
 .profile-image {
     width: 100%;
     object-fit: cover;
-    border-radius: 10px; /* Bordes redondeados para la imagen */
-    margin-bottom: 1em; /* Espacio entre la imagen y el texto */
+    border-radius: 10px;
+    /* Bordes redondeados para la imagen */
+    margin-bottom: 1em;
+    /* Espacio entre la imagen y el texto */
 }
 
 .professions {
@@ -47,6 +42,7 @@ main {
 }
 
 .professions p {
-    font-weight: bold; /* Hace que los títulos de las profesiones sean más destacados */
+    font-weight: bold;
+    /* Hace que los títulos de las profesiones sean más destacados */
 }
 </style>
